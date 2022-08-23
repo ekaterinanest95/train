fd165e5d348026d73b61fbf91ddf250853a8ee85
Ekaterina <katya.kinal@gmail.com>
2022-08-23 14:02:40 +0300

    Ex01

---
+++ Ex01/Program.cs
@@ -0,0 +1,47 @@
+﻿
+/*int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int i in numbers)
+{
+    Console.Write($"[{String.Join("; ", i)}]");
+}
+
+int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 }};
+ 
+int rows = numbers.GetUpperBound(0) + 1;    // количество строк
+int columns = numbers.Length / rows;        // количество столбцов
+// или так
+// int columns = numbers.GetUpperBound(1) + 1;
+ 
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{numbers[i, j]} \t");
+    }
+    Console.WriteLine();
+}*/
+
+
+
+int[][] numbers = new int[3][];
+numbers[0] = new int[] { 1, 2 };
+numbers[1] = new int[] { 1, 2, 3 };
+numbers[2] = new int[] { 1, 2, 3, 4, 5 };
+foreach(int[] row in numbers)
+{
+    foreach(int number in row)
+    {
+        Console.Write($"{number} \t");
+    }
+    Console.WriteLine();
+}
+ 
+// перебор с помощью цикла for
+for (int i = 0; i<numbers.Length;i++)
+{
+    for (int j =0; j<numbers[i].Length; j++)
+    {
+        Console.Write($"{numbers[i][j]} \t");
+    }
+    Console.WriteLine();
+}
